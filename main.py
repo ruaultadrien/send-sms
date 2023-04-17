74addd624cce81b0ef101123b56e30a0bf81ac2a
Adrien Ruault <ar@visium.ch>
2023-04-17 15:14:06 +0200

    first messaged sent

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-
-
 import vonage
 import os
 
 
 def main():
+    client = vonage.Client(
+        key=os.environ["VONAGE_API_KEY"], secret=os.environ["VONAGE_API_SECRET"]
+    )
+    sms = vonage.Sms(client)
+    response_data = sms.send_message(
+        {
+            "from": "J. Mollard",
+            "to": os.environ["TO_NUMBER"],
+            "text": "Bouge ton cul et va à la conf fiston !",
+        }
+    )
 
-    client = vonage.Client(key=os.environ['VONAGE_API_KEY'] , secret=os.environ['VONAGE_API_SECRET'])
-    client.sms.send_message({
-        "from": os.environ['VONAGE_BRAND_NAME'],
-        "to": os.environ['TO_NUMBER'],
-        "text": "A text message sent using the Vonage SMS API",
-    })
+    if response_data["messages"][0]["status"] == "0":
+        print("Message sent successfully.")
+    else:
+        print(
+            f"Message failed with error: {response_data['messages'][0]['error-text']}"
+        )
 
 
 if __name__ == "__main__":
